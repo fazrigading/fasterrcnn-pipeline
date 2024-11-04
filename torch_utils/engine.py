22e6bda58170e22bad58ce4e76006be26a98a6da
Fazri Gading <super.fai700@gmail.com>
2024-11-05 01:35:26 +0800

    restored evaluate and get_iou_types function in engine.py

--- torch_utils/engine.py
+++ torch_utils/engine.py
@@ -132,3 +132,78 @@ def train_one_epoch(
         batch_loss_objectness_list, 
         batch_loss_rpn_list
     )
+
+def _get_iou_types(model):
+    model_without_ddp = model
+    if isinstance(model, torch.nn.parallel.DistributedDataParallel):
+        model_without_ddp = model.module
+    iou_types = ["bbox"]
+    if isinstance(model_without_ddp, torchvision.models.detection.MaskRCNN):
+        iou_types.append("segm")
+    if isinstance(model_without_ddp, torchvision.models.detection.KeypointRCNN):
+        iou_types.append("keypoints")
+    return iou_types
+
+
+@torch.inference_mode()
+def evaluate(
+    model, 
+    data_loader, 
+    device, 
+    save_valid_preds=False,
+    out_dir=None,
+    classes=None,
+    colors=None
+):
+    n_threads = torch.get_num_threads()
+    # FIXME remove this and make paste_masks_in_image run on the GPU
+    torch.set_num_threads(1)
+    cpu_device = torch.device("cpu")
+    model.eval()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = "Test:"
+
+    coco = get_coco_api_from_dataset(data_loader.dataset)
+    iou_types = _get_iou_types(model)
+    coco_evaluator = CocoEvaluator(coco, iou_types)
+
+    counter = 0
+    for images, targets in metric_logger.log_every(data_loader, 100, header):
+        counter += 1
+        images = list(img.to(device) for img in images)
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        model_time = time.time()
+        outputs = model(images)
+
+        outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+        model_time = time.time() - model_time
+
+        res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
+        evaluator_time = time.time()
+        coco_evaluator.update(res)
+        evaluator_time = time.time() - evaluator_time
+        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
+
+        if save_valid_preds and counter == 1:
+            # The validation prediction image which is saved to disk
+            # is returned here which is again returned at the end of the
+            # function for WandB logging.
+            val_saved_image = save_validation_results(
+                images, outputs, counter, out_dir, classes, colors
+            )
+        elif save_valid_preds == False and counter == 1:
+            val_saved_image = np.ones((1, 64, 64, 3))
+            
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    coco_evaluator.synchronize_between_processes()
+
+    # accumulate predictions from all images
+    coco_evaluator.accumulate()
+    stats = coco_evaluator.summarize()
+    torch.set_num_threads(n_threads)
+    return stats, val_saved_image
